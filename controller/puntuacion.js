542c587b09501381f6c6c92d2feb153d728dff05
christianaulaestudio <christianaulaestudio@gmail.com>
2020-02-05 14:52:31 +0100

    refactorización completa del backend

---
+++ controller/puntuacion.js
@@ -0,0 +1,151 @@
+var Puntuacion = require("../models/puntuacion");
+
+//Comentado callbacks  , luego promesas , sin comentar  asyncway
+async function getAll(req, res) {
+  /* Puntuacion.find({}).exec((err, puntuaciones) => {
+      if (err) {
+        res
+          .status(500)
+          .send({ accion: "get all", mensaje: "error al obtener la puntuacion" });
+      } else {
+        res.status(200).send({ accion: "get all", datos: puntuaciones });
+      }
+    });*/
+
+  /*
+    Puntuacion.find({}).exec()
+    .then( puntuaciones =>   res.status(200).send({ accion: "get all", datos: puntuaciones }))
+    .catch(err =>   res.status(500).send({ accion: "get all", mensaje: `error al obtener la puntuacion ${err} ` }))
+  */
+
+  try {
+    let puntuaciones = await Puntuacion.find();
+    res.status(200).send({ accion: "get all", datos: puntuaciones });
+  } catch (err) {
+    res.status(500).send({ accion: "get all", mensaje: "error al obtener la puntuacion" });
+  }
+}
+
+
+//obtener una puntuacion filtramos por id
+async function getById(req, res) {
+
+//   let puntuacionId = req.params.id;
+  //Puntuacion.find({_id:puntuacionId})
+//   Puntuacion.findById(puntuacionId).exec((err, puntuacion) => {
+//     if (err) {
+//       res
+//         .status(500)
+//         .send({ accion: "get one", mensaje: "error al obtener la puntuacion" });
+//     } else {
+//       res.status(200).send({ accion: "get one", datos: puntuacion });
+//     }
+//   });
+
+
+  try {
+    let puntuacionId = req.params.id;
+    let puntuacion = await Puntuacion.findById(puntuacionId);
+    res.status(200).send({ accion: "get one", datos: puntuacion });
+  } catch (err) {
+    res.status(500).send({ accion: "get one", mensaje: "error al obtener la puntuacion" });
+  }
+
+}
+
+
+
+
+async function insert(req, res) {
+//   var datos = req.body;
+//   var puntuacion = new Puntuacion();
+//   puntuacion.nombre = datos.nombre;
+//   puntuacion.puntuacion = datos.puntuacion;
+//   puntuacion.save((err, puntuacionGuardada) => {
+//     if (err) {
+//       res
+//         .status(500)
+//         .send({ accion: "save", mensaje: "Error al guardar la puntuacion" });
+//     } else {
+//       res.status(500).send({ accion: "save", datos: puntuacionGuardada });
+//     }
+//   });
+
+try {
+    var puntuacion = new Puntuacion(req.body); // te guardas a ti mismo a traves del objeto 
+    let puntuacionGuardada = await puntuacion.save();
+    res.status(200).send({ accion: "save", datos: puntuacionGuardada });
+  } catch (err) {
+    res.status(500).send({ accion: "save", mensaje: "error al obtener la puntuacion" });
+  }
+
+
+
+
+}
+
+
+
+
+async function remove(req, res) {
+
+//   let puntuacionId = req.params.id;
+//   Puntuacion.findByIdAndDelete(puntuacionId, (err, puntuacionBorrada) => {
+//     if (err) {
+//       res
+//         .status(500)
+//         .send({ accion: "delete", mensaje: "Error al borrar la puntuacion" });
+//     } else if (!puntuacionBorrada) {
+//       res
+//         .status(404)
+//         .send({ accion: "delete", mensaje: "Error el id no existe" });
+//     } else {
+//       res.status(500).send({ accion: "delete", datos: puntuacionBorrada });
+//     }
+//   });
+
+try {
+    let puntuacionId = req.params.id;
+    let puntuacionBorrada = await Puntuacion.findByIdAndRemove(puntuacionId);
+    res.status(200).send({ accion: "remove", datos: puntuacionBorrada });
+  } catch (err) {
+    res.status(500).send({ accion: "remove", mensaje: "error al borrar la puntuacion" });
+  }
+
+}
+
+async function update(req, res) {
+    
+//   var datos = req.body;
+//   let puntuacionId = req.params.id;
+//   Puntuacion.findByIdAndUpdate(
+//     puntuacionId,
+//     datos,
+//     (err, puntuacioActualizada) => {
+//       if (err) {
+//         res.status(500).send({
+//           accion: "update",
+//           mensaje: "Error al actualizar la puntuacion"
+//         });
+//       } else if (!puntuacioActualizada) {
+//         res
+//           .status(404)
+//           .send({ accion: "update", mensaje: "Error el id no existe" });
+//       } else {
+//         res.status(500).send({ accion: "update", datos: puntuacioActualizada });
+//       }
+//     }
+//   );
+
+try {
+    var datos = req.body;
+    let puntuacionId = req.params.id;
+    let puntuacionActualizada = await Puntuacion.findByIdAndUpdate(puntuacionId , datos);
+    res.status(200).send({ accion: "update", datos: puntuacionActualizada });
+  } catch (err) {
+    res.status(500).send({ accion: "update", mensaje: "error al actualizar la puntuacion" });
+  }
+
+}
+
+module.exports = { getAll, getById, insert, remove, update };
--- index.js
+++ index.js
@@ -1,108 +1,39 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var Puntuacion = require("./models/puntuacion");
+var mongoose = require("mongoose"); //para mongo
+var routerPuntuacion = require('./routers/puntuacion')
+var morgan = require('morgan') // modulo para registrar las peticiones que nos van llegando desde la consola
+var cors = require('cors')
 var app = express();
+
 //transforma las peticiones de texto a json usando bodyParser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.status(200).send("hola");
-});
-//obtenemos todas las puntuaciones
-app.get("/puntuaciones/", (req, res) => {
-  Puntuacion.find({}).exec((err, puntuaciones) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ accion: "get all", mensaje: "error al obtener la puntuacion" });
-    } else {
-      res.status(200).send({ accion: "get all", datos: puntuaciones });
-    }
-  });
-  /* let datosJSON = {
-    accion: "get all",
-    datos: [
-      { nombre: "pepe", puntuacion: 33 },
-      { nombre: "bea", puntuacion: 23 },
-      { nombre: "felix", puntuacion: 29 }
-    ]
-  };
-  res.status(200).send(datosJSON);*/
-});
+app.use(cors())
+app.use(morgan('dev')) // solo para modo desarrollo con el 'dev'
 
-//obtener una puntuacion filtramos por id
-app.get("/puntuacion/:id", (req, res) => {
-  let puntuacionId = req.params.id;
-  //Puntuacion.find({_id:puntuacionId})
-  Puntuacion.findById(puntuacionId).exec((err, puntuacion) => {
-    if (err) {
-      res
-        .status(500)
-        .send({ accion: "get one", mensaje: "error al obtener la puntuacion" });
-    } else {
-      res.status(200).send({ accion: "get one", datos: puntuacion});
-    }
-  });
-});
+app.use('/puntuacion' , routerPuntuacion)
+
+//abro las puertas de cors , npm install cors asi nos lo ahorramos
+/*app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});*/
+
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("hola");
+// });
 
 
 
 
-//insertar una puntuacion
-app.post("/puntuacion", (req, res) => {
-  var datos = req.body;
-  var puntuacion = new Puntuacion();
-  puntuacion.nombre = datos.nombre;
-  puntuacion.puntuacion = datos.puntuacion;
-  puntuacion.save((err, puntuacionGuardada) => {
-    if (err) {
-      res.status(500).send({ accion: "save", mensaje: "Error al guardar la puntuacion" });
-    } else {
-      res.status(500).send({ accion: "save", datos: puntuacionGuardada });
-    }
-  });
-  /*let datosJsonRespuesta = {
-    accion: "save",
-    datos: datos
-  };
-  res.status(200).send(datosJsonRespuesta);*/
-});
-//eliminar una puntuacion
-app.delete("/puntuacion/:id", (req, res) => {
-  let puntuacionId = req.params.id;
-  Puntuacion.findByIdAndDelete(puntuacionId, (err , puntuacionBorrada)=>{
-    if(err){
-      res.status(500).send({ accion: "delete", mensaje: "Error al borrar la puntuacion" });
-     }else if(!puntuacionBorrada){
-        res.status(404).send({ accion: "delete", mensaje: "Error el id no existe" });
-    }else{
-      res.status(500).send({ accion: "delete", datos: puntuacionBorrada });
-    }
-  })
- /* let datosJsonRespuesta = {
-    accion: "delete",
-    datos: puntuacionId
-  };
-  res.status(200).send(datosJsonRespuesta);*/
-});
-//actualizar una puntuacion
-app.put('/puntuacion/:id' , (req ,res)=>{
-  var datos = req.body;
-  let puntuacionId = req.params.id;
-  Puntuacion.findByIdAndUpdate(puntuacionId , datos , (err , puntuacioActualizada)=>{
-    if(err){
-      res.status(500).send({ accion: "update", mensaje: "Error al actualizar la puntuacion" });
-    }else if(!puntuacioActualizada){
-      res.status(404).send({ accion: "update", mensaje: "Error el id no existe" });
-    }else{
-      res.status(500).send({ accion: "update", datos: puntuacioActualizada });
-    }
-  })
-})
-
-mongoose.connect('mongodb://localhost:27018/scores', (err, res) => {
+//Conexion db y arranque servidor
+/*mongoose.connect('mongodb://localhost:27018/scores', {useFindAndModify:true , useNewUrlParser: true , useUnifiedTopology: true } , (err, res) => {
   if (err) {
     console.log("error al conectarme a la bd");
   } else {
@@ -112,4 +43,12 @@ mongoose.connect('mongodb://localhost:27018/scores', (err, res) => {
       console.log("API REST funcionando en el puerto 5200 ");
     });
   }
-});
+});*/
+
+const run = async()=>{
+  await mongoose.connect('mongodb://localhost:27018/scores', {useFindAndModify:true , useNewUrlParser: true , useUnifiedTopology: true })
+  await app.listen(5200)
+  console.log("Servidor y db arrancados")
+}
+
+run().catch(err => console.err(`Fallo al arrancar : ${err}`));
